793f66377ad5bee86f1aeda8daf7dfd3f704f8ff
mr-wang1009 <1115378579@qq.com>
2022-07-21 14:24:23 +0800

    feat：add random img

---
+++ src/components/Image.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const imageOptions = ref({ src: 'https://place.dog/300/200' })
+const { isLoading } = useImage(imageOptions, { delay: 2000 })
+</script>
+
+<template>
+  <div v-if="isLoading" class="w-[300px] h-[200px] animate-pulse bg-gray-500/5 p-2" />
+  <img v-else :src="imageOptions.src" class="w-[300px] h-[200px]">
+</template>
+
